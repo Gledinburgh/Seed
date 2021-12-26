e56be286de7676f90f00791758d4b008459208cc
Gledinburgh <gledinburgh@gmail.com>
2021-12-25 20:31:49 -0600

    added member detail json

--- components/MemberList.js
+++ components/MemberList.js
@@ -3,39 +3,54 @@ const { Header, Content, Footer } = Layout;
 import Image from 'next/image';
 
 import styles from '../styles/Explore.module.css';
+import { memberDetails } from '../data/memberDetails';
 
 import { useEffect, useState, useContext } from 'react';
 import { GeneralContext } from '../Context/GeneralContext';
 
 
 export default function MemberList() {
+
+  const panelPath = "/membersList/Panel/"
+  const profilePath = "/membersList/Profile/"
   const context = GeneralContext();
 
   useEffect(() => {
     console.log("useEffect: Members")
     context.setTitle("Members");
 
+
   }, [])
 
   return (
 
+
     <Content>
-      <Row className={styles.row}>
-        < Image
-          className={styles.Image}
-          src="/Placeholder_1.jpg"
-          layout="fill"
-          objectFit="cover"
-        />
-      </Row>
-      <Row className={styles.row}>
-        < Image
-          className={styles.Image}
-          src="/Placeholder_2.jpg"
-          layout="fill"
-          objectFit="cover"
-        />
-      </Row>
+      {
+        memberDetails.map(member => {
+          console.log('Panel Path', `${panelPath}.${member.panel}`);
+          return (
+
+            <Row className={styles.row}>
+              <div className={styles.name}>
+                <span className={styles["name-text"]}>
+                  {member.member}
+                </span>
+
+              </div>
+
+
+              <Image
+                className={styles.Image}
+                src={`${panelPath}${member.panel}`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </Row>
+          )
+        })
+      }
+
     </Content>
 
 
---
+++ data/memberDetails.js
@@ -0,0 +1,56 @@
+export const memberDetails = [
+  {
+    "member": "Powdered Wig Machine",
+    "panel": "Brandon.jpg",
+    "alt": "Image of cosutumed performers speaking on stage",
+    "profile": null
+  },
+  {
+    "member": "Hated Bodies",
+    "panel": "Josh.jpg",
+    "alt": "Image of Musicians playing hardcore music",
+    "profile": null
+  },
+  {
+    "member": "Mauro de la Tierra",
+    "panel": "Placeholder_2.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "Sheila Marie",
+    "panel": "Placeholder_3.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "Mia Santiago",
+    "panel": "Placeholder_1.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "The Wizard",
+    "panel": "Placeholder_2.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "Marlin Bogle",
+    "panel": "Placeholder_3.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "Glenn Edinburgh",
+    "panel": "Placeholder_1.jpg",
+    "alt": "place holder image",
+    "profile": null
+  },
+  {
+    "member": "Raven M.",
+    "panel": "Placeholder_2.jpg",
+    "alt": "place holder image",
+    "profile": null
+  }
+]
