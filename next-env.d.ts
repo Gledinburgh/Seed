42eab321e440d33e9408dd066c84db630b576345
Gledinburgh <gledinburgh@gmail.com>
2022-01-17 14:44:49 -0600

    added css as typescript type

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,10 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
